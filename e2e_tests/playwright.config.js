d4afe73f1f55c07b2569d7c231aaf2eec2cf6fdc
Peksford <juhapekkaj.virtanen@gmail.com>
2025-03-13 15:45:30 +0200

    a

--- e2e_tests/playwright.config.js
+++ e2e_tests/playwright.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:5173/',
+    // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -74,12 +74,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm --prefix ../backend run start:test',
-    url: 'http://localhost:3001/api/health',
-    reuseExistingServer: !process.env.CI,
-    timeout: 30000,
-    stderr: 'inherit',
-    stdout: 'inherit',
-  },
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://127.0.0.1:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
 });
