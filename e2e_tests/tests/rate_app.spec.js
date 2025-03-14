5ad1a33d79ac4a736bbc87551c59da2c0a704140
Peksford <juhapekkaj.virtanen@gmail.com>
2025-03-14 23:59:59 +0200

    e

--- e2e_tests/tests/rate_app.spec.js
+++ e2e_tests/tests/rate_app.spec.js
@@ -29,9 +29,9 @@ describe('Rate app', () => {
   });
   test('user can log in', async ({ page, request }) => {
     await test.step('user can log in', async () => {
-      console.log('what is page?', page);
+      console.log('what is page?', await page);
       const response = await request.get('http://localhost:3001/api/health');
-      console.log('Backend health check: ', response.status());
+      console.log('Backend health check: ', await response.status());
       await loginWith(page, 'kayttaja', 'salasana');
       await page.waitForNavigation({ waitUntil: 'load' });
       await page.waitForSelector('text=Welcome kayttaja');
