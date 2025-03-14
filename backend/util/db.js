ff7c326ecaf5078596941cae642cc5018ba7ab5b
Peksford <juhapekkaj.virtanen@gmail.com>
2025-03-14 11:39:26 +0200

    db.js change

--- backend/util/db.js
+++ backend/util/db.js
@@ -2,7 +2,10 @@ const Sequelize = require('sequelize');
 const { DATABASE_URL } = require('./config');
 const { Umzug, SequelizeStorage } = require('umzug');
 
-const sequelize = new Sequelize(DATABASE_URL);
+const sequelize = new Sequelize(DATABASE_URL, {
+  dialect: 'postgres',
+  logging: false,
+});
 
 const connectToDatabase = async () => {
   try {
