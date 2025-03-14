ac9fc4862918060a3281877e735978d5f3725886
Peksford <juhapekkaj.virtanen@gmail.com>
2025-03-14 20:59:41 +0200

    commit

--- e2e_tests/tests/rate_app.spec.js
+++ e2e_tests/tests/rate_app.spec.js
@@ -11,11 +11,11 @@ describe('Rate app', () => {
         password: 'salasana',
       },
     });
-    await page.goto('http://localhost:5173/login');
+    await page.goto('http://[::1]:5173/login');
   });
 
   test('front page can be opened', async ({ page }) => {
-    await page.goto('http://localhost:5173/kayttaja');
+    await page.goto('http://[::1]:5173/kayttaja');
 
     const locator = await page.getByText(
       'kayttaja has not reviewed anything yet!'
@@ -23,10 +23,10 @@ describe('Rate app', () => {
     await expect(locator).toBeVisible();
     await expect(page.getByText('albums')).toBeVisible();
   });
-  test('user can log in', async ({ page }) => {
-    await loginWith(page, 'kayttaja', 'salasana');
-    await expect(page.getByText('Welcome kayttaja')).toBeVisible();
-  });
+  // test('user can log in', async ({ page }) => {
+  //   await loginWith(page, 'kayttaja', 'salasana');
+  //   await expect(page.getByText('Welcome kayttaja')).toBeVisible();
+  // });
 });
 //   test('login fails with wrong password', async ({ page }) => {
 //     await loginWith(page, 'kayttaja', 'wrong');
