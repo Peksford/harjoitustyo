9839fd4318a03f6abb924addf518052e1fc9004c
Peksford <juhapekkaj.virtanen@gmail.com>
2025-03-14 18:02:14 +0200

    once again

--- e2e_tests/tests/rate_app.spec.js
+++ e2e_tests/tests/rate_app.spec.js
@@ -3,7 +3,10 @@ const { loginWith, searchObject } = require('./helper');
 
 describe('Rate app', () => {
   beforeEach(async ({ page, request }) => {
-    await request.post('http://127.0.0.1:3001/api/testing/reset');
+    let response = await request.post(
+      'http://127.0.0.1:3001/api/testing/reset'
+    );
+    console.log('Reset response', await response.text());
     await request.post('http://127.0.0.1:3001/api/users', {
       data: {
         username: 'kayttaja',
