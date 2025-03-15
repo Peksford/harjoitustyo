8ce1b47c36ec13ecf1c4b306e8f06686ec2f3bfe
Peksford <juhapekkaj.virtanen@gmail.com>
2025-03-15 10:44:14 +0200

    bada bastu

--- e2e_tests/playwright.config.js
+++ e2e_tests/playwright.config.js
@@ -76,7 +76,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: [
     {
-      command: 'cd ../backend && npm run start:test',
+      command: 'cd ../backend && npm run start:test &',
       url: 'http://127.0.0.1:3001',
       timeout: 120 * 1000,
       reuseExistingServer: !process.env.CI,
