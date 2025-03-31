f25d2918df4f595fca5b0357f1c1b7a5d13d3c74
Peksford <juhapekkaj.virtanen@gmail.com>
2025-03-31 17:29:49 +0300

    cron job e2e

--- backend/cronjob.js
+++ backend/cronjob.js
@@ -1,20 +1,23 @@
 const cron = require('node-cron');
 const { Album, Book, Movie, Game } = require('./models');
 
-cron.schedule(
-  '*/1 * * * *',
-  async () => {
-    try {
-      console.log('Setting up cronjob...');
-      await Album.update({ heart: false }, { where: {} });
-      await Book.update({ heart: false }, { where: {} });
-      await Movie.update({ heart: false }, { where: {} });
-      await Game.update({ heart: false }, { where: {} });
-    } catch (error) {
-      console.error('Error with heart cronjob');
+if (process.env.NODE_ENV !== 'test') {
+  console.log('Setting up cronjob...');
+  cron.schedule(
+    '*/1 * * * *',
+    async () => {
+      try {
+        console.log('Cron job running...');
+        await Album.update({ heart: false }, { where: {} });
+        await Book.update({ heart: false }, { where: {} });
+        await Movie.update({ heart: false }, { where: {} });
+        await Game.update({ heart: false }, { where: {} });
+      } catch (error) {
+        console.error('Error with heart cronjob');
+      }
+    },
+    {
+      timezone: 'Europe/Helsinki',
     }
-  },
-  {
-    timezone: 'Europe/Helsinki',
-  }
-);
+  );
+}
